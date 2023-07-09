4bfcd56c61338dfea1229e37007dc5f6d87250ef
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-09 20:10:34 +0200

    generate authmodule.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseModuleConfig } from './config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRootAsync(mongooseModuleConfig)],
+  imports: [MongooseModule.forRootAsync(mongooseModuleConfig), AuthModule],
 })
 export class AppModule {}
