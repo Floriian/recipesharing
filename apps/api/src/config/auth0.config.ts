1ebe3f3bf396070ea64eb6794cf092e43f85e30b
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 16:55:21 +0200

    add type for auth0Config registerAs method.

--- apps/api/src/config/auth0.config.ts
+++ apps/api/src/config/auth0.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export const auth0Config = registerAs('auth0Config', () => ({
+export const auth0Config = registerAs<IAuth0Config>('auth0Config', () => ({
   AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
   AUTH0_ISSUER_URL: process.env.AUTH0_ISSUER_URL,
 }));
