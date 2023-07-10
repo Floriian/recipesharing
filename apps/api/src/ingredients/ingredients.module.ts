d353c99fb8a678579e95c7b3a642ebf33c43389d
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 23:47:34 +0200

    inject ingredient model to ingredient module.

--- apps/api/src/ingredients/ingredients.module.ts
+++ apps/api/src/ingredients/ingredients.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { IngredientsController } from './ingredients.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ingredient, IngredientSchema } from './schema/Ingredient.schema';
 
 @Module({
   controllers: [IngredientsController],
-  providers: [IngredientsService]
+  providers: [IngredientsService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Ingredient.name,
+        schema: IngredientSchema,
+      },
+    ]),
+  ],
 })
 export class IngredientsModule {}
