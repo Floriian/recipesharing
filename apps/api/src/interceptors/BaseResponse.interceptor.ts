8a9b57cd314cc2b8c1cdeb83f9f994359a42bc09
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 16:22:43 +0200

    create response interceptor

---
+++ apps/api/src/interceptors/BaseResponse.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, catchError, map, throwError } from 'rxjs';
+
+export class BaseResponseInterceptor implements NestInterceptor {
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        return { success: true, data };
+      }),
+      catchError((error: any) => {
+        return throwError(() => {
+          if (error instanceof HttpException) {
+            throw new HttpException(
+              {
+                success: false,
+                data: {
+                  message: error.message,
+                  statusCode: error.getStatus(),
+                },
+              },
+              error.getStatus(),
+            );
+          } else {
+            return throwError(() => {
+              throw error;
+            });
+          }
+        });
+      }),
+    );
+  }
+}
