43e2e7093532a97714b7ee3fd2b81ad8c31471ba
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 12:32:00 +0200

    inject config and authmodule to main moduel

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseModuleConfig } from './config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRootAsync(mongooseModuleConfig), AuthModule],
+  imports: [
+    MongooseModule.forRootAsync(mongooseModuleConfig),
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
