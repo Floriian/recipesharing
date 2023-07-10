4d557bf6e8dadaa43f00d21c87056ae1005fb38b
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 14:38:58 +0200

    create app.

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { auth0Config } from '../config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [ConfigModule.forFeature(auth0Config)],
+  imports: [
+    ConfigModule.forFeature(auth0Config),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
