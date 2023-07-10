692e052fe427aa7f605e7e2f55beeb37c6acf7d1
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-10 14:41:32 +0200

    create recipes module.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { mongooseModuleConfig } from './config';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync(mongooseModuleConfig),
     AuthModule,
     ConfigModule.forRoot(),
+    RecipesModule,
   ],
 })
 export class AppModule {}
