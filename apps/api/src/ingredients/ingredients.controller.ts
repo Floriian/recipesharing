fba777dc22d9d4284678bdabe35a926d88606ddd
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 00:08:37 +0200

    use guards on post, patch, delete methods.

--- apps/api/src/ingredients/ingredients.controller.ts
+++ apps/api/src/ingredients/ingredients.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import {
@@ -13,6 +14,7 @@ import {
   IngredientServiceActions,
   UpdateIngredientDto,
 } from '@recipe-sharing/types';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('ingredients')
 export class IngredientsController implements IngredientServiceActions {
@@ -28,16 +30,19 @@ export class IngredientsController implements IngredientServiceActions {
     return this.ingredientsService.getIngredient(name);
   }
 
+  @UseGuards(JwtGuard)
   @Post()
   createIngredient(@Body() dto: CreateIngredientDto) {
     return this.ingredientsService.createIngredient(dto);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(':id')
   updateIngredient(@Param('id') id: string, @Body() dto: UpdateIngredientDto) {
     return this.ingredientsService.updateIngredient(id, dto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   deleteIngredient(@Param('id') id: string) {
     return this.ingredientsService.deleteIngredient(id);
