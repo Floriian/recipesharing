40e96ea3cd7da1a9930995e9b726abd166b7314a
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 00:06:48 +0200

    use global interceptor

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipeConfig } from './config';
+import { BaseResponseInterceptor } from './interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
+
+  app.useGlobalInterceptors(new BaseResponseInterceptor());
 
   app.enableCors();
 
