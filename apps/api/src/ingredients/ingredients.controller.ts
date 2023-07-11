b5628b57878a364c8e2950b3ac7cf35ff7498890
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-11 14:26:57 +0200

    create ingredients controller.

--- apps/api/src/ingredients/ingredients.controller.ts
+++ apps/api/src/ingredients/ingredients.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  CreateIngredientDto,
+  IngredientServiceActions,
+  UpdateIngredientDto,
+} from '@recipe-sharing/types';
 import { IngredientsService } from './ingredients.service';
 
 @Controller('ingredients')
-export class IngredientsController {
+export class IngredientsController implements IngredientServiceActions {
   constructor(private readonly ingredientsService: IngredientsService) {}
+
+  @Get()
+  getIngredients() {
+    return this.ingredientsService.getIngredients();
+  }
+
+  @Get(':name')
+  getIngredient(@Param('name') name: string) {
+    return this.ingredientsService.getIngredient(name);
+  }
+
+  @Post()
+  createIngredient(dto: CreateIngredientDto) {
+    return this.ingredientsService.createIngredient(dto);
+  }
+  @Patch(':id')
+  updateIngredient(@Param('id') id: string, dto: UpdateIngredientDto) {
+    return this.ingredientsService.updateIngredient(id, dto);
+  }
+  @Delete(':id')
+  deleteIngredient(@Param('id') id: string) {
+    return this.ingredientsService.deleteIngredient(id);
+  }
 }
