079612dff43e19337d1d7a755371abd963269127
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 01:03:15 +0200

    create recipe schema.

--- apps/api/src/recipes/recipes.module.ts
+++ apps/api/src/recipes/recipes.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RecipesService } from './recipes.service';
 import { RecipesController } from './recipes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Recipe, RecipeSchema } from './schema/Recipe.schema';
 
 @Module({
   controllers: [RecipesController],
-  providers: [RecipesService]
+  providers: [RecipesService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Recipe.name,
+        schema: RecipeSchema,
+      },
+    ]),
+  ],
 })
 export class RecipesModule {}
---
+++ apps/api/src/recipes/schema/Recipe.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IIngredient, IRecipe } from '@recipe-sharing/types';
+import mongoose, { HydratedDocument, Model } from 'mongoose';
+import { Ingredient } from '../../ingredients/schema/Ingredient.schema';
+
+@Schema()
+export class Recipe implements IRecipe {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  kcal: number;
+  @Prop({ default: false })
+  glutenFree: boolean;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Ingredient.name })
+  ingredients: IIngredient[];
+
+  @Prop({ required: true })
+  description: string;
+}
+
+export type RecipeDocument = HydratedDocument<Recipe>;
+export type RecipeModel = Model<Recipe>;
+export const RecipeSchema = SchemaFactory.createForClass(Recipe);
