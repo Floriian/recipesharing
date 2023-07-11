008a21576beadd9d96581ee781ccdc85b1d71dfd
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 00:57:01 +0200

    refactor exporting in types lib.

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,5 +1,2 @@
-export * from "./ingredient/Ingredient";
-export * from "./ingredient/IngredientServiceActions";
-export * from "./ingredient/UpdateIngredient.dto";
-export * from "./ingredient/CreateIngredient.dto";
+export * from "./ingredient";
 export * from "./recipes";
---
+++ libs/types/src/ingredient/index.ts
@@ -0,0 +1,4 @@
+export * from "./CreateIngredient.dto";
+export * from "./Ingredient";
+export * from "./IngredientServiceActions";
+export * from "./UpdateIngredient.dto";
