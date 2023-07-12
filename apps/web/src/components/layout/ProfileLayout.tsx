1413e4f9467519be1e3cb2ffe02abd2169bb9e10
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 17:00:15 +0200

    profile layout auth.

--- apps/web/src/components/layout/ProfileLayout.tsx
+++ apps/web/src/components/layout/ProfileLayout.tsx
@@ -1,7 +1,11 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export function ProfileLayout() {
+  const { isAuthenticated } = useAuth0();
+  if (!isAuthenticated) return <Navigate to="/" />;
+
   return (
     <div>
       profile layout
