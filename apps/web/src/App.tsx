f3ee3f4b4a993d2478fe8ee34db90a7d66ea0ab0
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 16:24:30 +0200

    prepare for profile page.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Layout } from "./components/layout/Layout";
+import { ProfileLayout, Layout } from "./components";
 export default function App() {
   return (
     <Routes>
@@ -8,7 +8,11 @@ export default function App() {
         <Route
           path="/"
           element={<h1 className="text-red-500">Hello World</h1>}
-        ></Route>
+        />
+
+        <Route element={<ProfileLayout />} path="/profile">
+          <Route index element={<h1>ASDJKASd</h1>} />
+        </Route>
       </Route>
     </Routes>
   );
