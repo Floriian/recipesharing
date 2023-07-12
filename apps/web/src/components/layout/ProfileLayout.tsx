3f2773aa7381dc48262f4f9b81d010ab9a6a45be
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 17:41:38 +0200

    add profile side menu to profile layout

--- apps/web/src/components/layout/ProfileLayout.tsx
+++ apps/web/src/components/layout/ProfileLayout.tsx
@@ -1,14 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { ProfileSideMenu } from "./ProfileSideMenu";
 
 export function ProfileLayout() {
   const { isAuthenticated } = useAuth0();
   if (!isAuthenticated) return <Navigate to="/" />;
 
   return (
-    <div>
-      profile layout
+    <div className="flex">
+      <ProfileSideMenu />
       <Outlet />
     </div>
   );
