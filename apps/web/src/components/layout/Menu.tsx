175766b72115125b75e6a7542660682def2581d0
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-12 19:19:20 +0200

    basic menu, intsall radixui reactiocns.

--- apps/web/src/components/layout/Menu.tsx
+++ apps/web/src/components/layout/Menu.tsx
@@ -2,8 +2,30 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { ProfileMenu } from "./ProfileMenu";
 import { AuthMenu } from "./AuthMenu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../ui/navigation-menu";
+import { Link } from "react-router-dom";
 
 export function Menu() {
   const { isAuthenticated } = useAuth0();
-  return <div>{isAuthenticated ? <ProfileMenu /> : <AuthMenu />}</div>;
+  return (
+    <nav className="flex w-full justify-end">
+      <div className="flex w-[50vw] justify-between items-center p-2">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink>
+                <Link to="/">Homepage</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        {isAuthenticated ? <ProfileMenu /> : <AuthMenu />}
+      </div>
+    </nav>
+  );
 }
