f48974acda12595ece0530c3237f11e51eff17d4
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-13 17:59:29 +0200

    create user schema.

---
+++ apps/api/src/user/schema/User.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IRecipe, IUser } from '@recipe-sharing/types';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Recipe } from '../../recipes/schema/Recipe.schema';
+
+@Schema()
+export class User implements IUser {
+  @Prop({ required: true })
+  sub: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Recipe.name })
+  recipes: IRecipe[];
+}
+
+export type UserDocument = HydratedDocument<User>;
+export type UserModel = Model<User>;
+export const UserSchema = SchemaFactory.createForClass(User);
