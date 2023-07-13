fc53b5436052e22fb5179da3948ce5c355d32160
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-13 17:57:16 +0200

    create user module.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { RecipesModule } from './recipes/recipes.module';
     ConfigModule.forRoot(),
     IngredientsModule,
     RecipesModule,
+    UserModule,
   ],
 })
 export class AppModule {}
