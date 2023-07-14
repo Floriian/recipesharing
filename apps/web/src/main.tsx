dbb7c6caa6a523e4384a65f06f6ad6958b9a2294
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-14 23:35:10 +0200

    configure router.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "@/app/store/store.ts";
 import { Auth0Context } from "@/contexts/Auth0Context";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Context>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </Provider>
     </Auth0Context>
   </React.StrictMode>
