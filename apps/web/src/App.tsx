38295a1b934abf5cd5d3c723c60a85e796ecced4
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 12:26:37 +0200

    add recipe routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,7 @@
 import { Home } from "@/components/home/Home";
 import { Layout } from "@/components/layout/Layout";
+import { RecipeLayout } from "@/features/Recipe/components/RecipeLayout";
+import { RecipePost } from "@/features/Recipe/components/RecipePost";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
@@ -8,6 +10,9 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<RecipeLayout />}>
+          <Route index element={<RecipePost />} />
+        </Route>
       </Route>
     </Routes>
   );
