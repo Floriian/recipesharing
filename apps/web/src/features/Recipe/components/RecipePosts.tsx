68843c2e4cd17a5e0132ae5b1037bd89a2aa6850
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 13:57:57 +0200

    useMemo for recipelist

--- apps/web/src/features/Recipe/components/RecipePosts.tsx
+++ apps/web/src/features/Recipe/components/RecipePosts.tsx
@@ -1,21 +1,34 @@
 import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
+import { Loading } from "@/components/loading/Loading";
+import { RecipeLayout } from "@/features/Recipe/components/RecipeLayout";
+import { RecipePost } from "@/features/Recipe/components/RecipePost";
 import { getRecipeThunk } from "@/features/Recipe/recipe.thunks";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export function RecipePosts() {
   const dispatch = useAppDispatch();
   const recipe = useAppSelector((state) => state.recipe);
   useEffect(() => {
-    return () => {
-      dispatch(getRecipeThunk());
-    };
-  }, [dispatch]);
+    dispatch(getRecipeThunk());
+  }, []);
+
+  const recipeList = useMemo(() => {
+    if (recipe.isLoading) {
+      return <Loading />;
+    }
+
+    return (
+      <>
+        {recipe.data.map((recipe) => (
+          <RecipePost recipe={recipe} key={recipe.description} />
+        ))}
+      </>
+    );
+  }, [recipe.isLoading]);
 
   return (
-    <div className="border border-secondary">
-      {recipe.data.map((recipe) => (
-        <div key={recipe.name}>{recipe.name}</div>
-      ))}
-    </div>
+    <RecipeLayout>
+      <div className="border border-secondary max-w-2xl">{recipeList}</div>
+    </RecipeLayout>
   );
 }
