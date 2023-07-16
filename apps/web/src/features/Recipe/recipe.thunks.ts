75864e0dd6f32b6ab3c57a932263a008a71abfb6
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 12:27:28 +0200

    create get recipe thunk

---
+++ apps/web/src/features/Recipe/recipe.thunks.ts
@@ -0,0 +1,21 @@
+import { recipeService } from "@/services/recipe.service";
+import { BaseResponse } from "@/types";
+import { IRecipe } from "@recipe-sharing/types";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getRecipeThunk = createAsyncThunk<BaseResponse<IRecipe[]>, void>(
+  "recipes/getRecipes",
+  async (_, api) => {
+    try {
+      const res = await recipeService.getRecipes();
+      return res.data;
+    } catch (e) {
+      console.log(e);
+      const { rejectWithValue } = api;
+      return rejectWithValue({
+        error: e,
+        success: false,
+      });
+    }
+  }
+);
