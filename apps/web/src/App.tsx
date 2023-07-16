42d3655bcbd930df11dc83ecbd200d184de23029
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 13:56:57 +0200

    fix layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,7 @@ import { Home } from "@/components/home/Home";
 import { Layout } from "@/components/layout/Layout";
 import { RecipeLayout } from "@/features/Recipe/components/RecipeLayout";
 import { RecipePost } from "@/features/Recipe/components/RecipePost";
+import { RecipePosts } from "@/features/Recipe/components/RecipePosts";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
@@ -10,8 +11,8 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<RecipeLayout />}>
-          <Route index element={<RecipePost />} />
+        <Route path="/recipes">
+          <Route index element={<RecipePosts />} />
         </Route>
       </Route>
     </Routes>
