a059fbaa1620b51476ee852fdbc199c2e280c26d
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 15:18:57 +0200

    fix loading component

--- apps/web/src/components/loading/Loading.tsx
+++ apps/web/src/components/loading/Loading.tsx
@@ -1,10 +1,8 @@
 import { SymbolIcon } from "@radix-ui/react-icons";
 export function Loading() {
   return (
-    <div className="border border-secondary rounded-sm flex-justify-center p-2 text-secondary flex-col">
-      <div className="animate-spin">
-        <SymbolIcon />
-      </div>
+    <div className="border border-secondary rounded-sm flex justify-center p-4 flex-col">
+      <SymbolIcon className="animate-spin w-16 h-16" />
       <p>Loading...</p>
     </div>
   );
