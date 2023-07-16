9e944730c7e46bf626377956a22a40f4f0f6d99d
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 20:51:04 +0200

    add ingredientreducer to rootreducer

--- apps/web/src/app/store/reducer.ts
+++ apps/web/src/app/store/reducer.ts
@@ -1,7 +1,9 @@
+import { ingredientsSlice } from "@/features/Ingredients/ingredients.slice";
 import { recipeSlice } from "@/features/Recipe/recipe.slice";
 import { authenticationSlice } from "@/features/authentication/authenticationSlice";
 import { combineReducers } from "@reduxjs/toolkit";
 export const rootReducer = combineReducers({
   [recipeSlice.name]: recipeSlice.reducer,
+  [ingredientsSlice.name]: ingredientsSlice.reducer,
   [authenticationSlice.name]: authenticationSlice.reducer,
 });
