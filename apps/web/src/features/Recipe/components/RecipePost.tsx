591ee1d4142c1be5c549d0fc1620679d8a4146c6
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 13:57:26 +0200

    basic recipe post card

--- apps/web/src/features/Recipe/components/RecipePost.tsx
+++ apps/web/src/features/Recipe/components/RecipePost.tsx
@@ -1,3 +1,37 @@
-export function RecipePost() {
-  return <h1>RecipePost</h1>;
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { IRecipe } from "@recipe-sharing/types";
+
+interface Props {
+  recipe: IRecipe;
+}
+
+export function RecipePost({ recipe }: Props) {
+  const { createdAt, description, glutenFree, ingredients, kcal, name } =
+    recipe;
+
+  const isLongDescription = description.length > 150;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>
+          {isLongDescription
+            ? `${description.substring(0, 150)}...`
+            : description}
+        </p>
+      </CardContent>
+      <CardFooter className="text-muted">
+        {/* <p>{ingredients.length}</p> */}
+        <p>{glutenFree}</p>
+      </CardFooter>
+    </Card>
+  );
 }
