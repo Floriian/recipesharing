aca08eade65947a76486f1c2b788d5f905f8d195
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 20:28:53 +0200

    create basic create recipe form

---
+++ apps/web/src/features/Recipe/components/CreateRecipeForm.tsx
@@ -0,0 +1,27 @@
+import { CreateIngredientDto, CreateRecipeDto } from "@recipe-sharing/types";
+import { useForm } from "react-hook-form";
+import { classValidatorResolver } from "@hookform/resolvers/class-validator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+export function CreateRecipeForm() {
+  const form = useForm<CreateRecipeDto>({
+    resolver: classValidatorResolver(CreateIngredientDto),
+  });
+
+  const handleSubmit = async (data: CreateRecipeDto) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={form.handleSubmit(handleSubmit)}
+      className="grid grid-cols-1 gap-2"
+    >
+      <Input {...form.register("name")} placeholder="Recipe name" />
+      <Textarea {...form.register("description")} placeholder="Desciption" />
+
+      <Button type="submit">Create Recipe</Button>
+    </form>
+  );
+}
