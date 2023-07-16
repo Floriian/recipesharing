d3964080b25d783745a69df3c0a436f7dec94db7
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-16 18:26:16 +0200

    checkbox

--- apps/web/src/features/Recipe/components/RecipePage.tsx
+++ apps/web/src/features/Recipe/components/RecipePage.tsx
@@ -1,5 +1,6 @@
 import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import { Loading } from "@/components/loading/Loading";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { RecipeCard } from "@/features/Recipe/components/RecipeCard";
 import { getRecipeThunk } from "@/features/Recipe/recipe.thunks";
@@ -9,6 +10,7 @@ import { useEffect, useMemo, useState } from "react";
 export function RecipePage() {
   const [search, setSearch] = useState<string>("");
   const [recipe, setRecipe] = useState<IRecipe[]>();
+  const [glutenFree, setGlutenFree] = useState<boolean>(false);
 
   const recipeState = useAppSelector((state) => state.recipe);
   const dispatch = useAppDispatch();
@@ -44,14 +46,25 @@ export function RecipePage() {
     );
   }, [recipeState.isLoading, search]);
 
+  const handleCheckbox = () => {
+    setGlutenFree(!glutenFree);
+  };
+
   return (
     <>
       <div className="flex w-full justify-center">
-        <Input
-          placeholder="Type to search"
-          className="w-96"
-          onChange={(e) => setSearch(e.currentTarget.value)}
-        />
+        <div>
+          {/* //TODO FINISH CHECKBOx */}
+          <Input
+            placeholder="Type to search"
+            className="w-96"
+            onClick={(e) => setSearch(e.currentTarget.value)}
+          />
+          <div className="flex items-center gap-1.5">
+            <Checkbox id="glutenfree" onChange={handleCheckbox} />
+            <label htmlFor="glutenfree">Gluten free</label>
+          </div>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-2">
         {recipeList}
