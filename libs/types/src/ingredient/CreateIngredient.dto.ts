784edb02cd9f8e47bcbf45df7dbc78c9347d23e5
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-17 18:55:09 +0200

    omit _id

--- libs/types/src/ingredient/CreateIngredient.dto.ts
+++ libs/types/src/ingredient/CreateIngredient.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from "class-validator";
 import { IIngredient } from "./Ingredient";
 
-export class CreateIngredientDto implements IIngredient {
+export class CreateIngredientDto implements Omit<IIngredient, "_id"> {
   @IsString()
   @IsNotEmpty()
   ingredients: string;
