fd175d68e974d337fcec9f385cea48598ce4d1b2
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-18 12:37:35 +0200

    remove bearer

--- apps/web/src/services/axiosInstace.ts
+++ apps/web/src/services/axiosInstace.ts
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:3000",
-  headers: {
-    Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
-  },
 });
