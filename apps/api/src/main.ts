ef03f4ad30f0aaf6158a5e481f297e3c27822b8d
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-20 23:56:15 +0200

    add some logging

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { validationPipeConfig } from './config';
-import { BaseResponseInterceptor } from './interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
 
-  app.useGlobalInterceptors(new BaseResponseInterceptor());
-
   app.enableCors();
 
-  await app.listen(3000);
+  const port = 3000 || process.env.PORT;
+
+  await app.listen(port);
+  Logger.log(`Server is listening at port ${port}`);
 }
 bootstrap();
