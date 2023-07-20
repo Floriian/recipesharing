d3fdc79f5d4afe45af928432e169cb280974b8fb
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-20 23:59:33 +0200

    finishing recipeform

--- apps/web/src/features/Recipe/components/CreateRecipeForm.tsx
+++ apps/web/src/features/Recipe/components/CreateRecipeForm.tsx
@@ -7,13 +7,24 @@ import { Textarea } from "@/components/ui/textarea";
 import { useEffect } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { recipeService } from "@/services/recipe.service";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 export function CreateRecipeForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<CreateRecipeDto>({
+  const form = useForm<CreateRecipeDto>({
     resolver: classValidatorResolver(CreateRecipeDto),
+    defaultValues: {
+      glutenFree: false,
+      description: "",
+      ingredients: "",
+      kcal: "0", //Because CreateRecipeDto kcal field is @IsNumberString() and it needs to be string.
+      name: "",
+    },
   });
 
   const submitData = async (data: CreateRecipeDto) => {
@@ -25,26 +36,87 @@ export function CreateRecipeForm() {
     }
   };
 
-  useEffect(() => console.log(errors), [errors]);
+  // useEffect(() => console.log(errors), [errors]);
 
   return (
-    <form
-      onSubmit={handleSubmit(submitData)}
-      className="grid grid-cols-1 gap-2"
-    >
-      <Input {...register("name")} placeholder="Recipe name" />
-      <Textarea {...register("description")} placeholder="Desciption" />
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(submitData)}
+        className="grid grid-cols-1 gap-2"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Recipe name</FormLabel>
+              <FormControl>
+                <>
+                  <Input placeholder="Recipe name" {...field} />
+                </>
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-      <Textarea {...register("ingredients")} placeholder="Ingredients" />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Gluten-Free</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Description" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <Input type="number" {...register("kcal")} placeholder="KCAL" />
-        <div className="flex items-center gap-1.5">
-          <Checkbox id="glutenfree" {...register("glutenFree")} />
-          <label htmlFor="glutenfree">Gluten free</label>
+        <FormField
+          control={form.control}
+          name="ingredients"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ingredients</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Description" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
+          <FormField
+            control={form.control}
+            name="kcal"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Kalories</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="kcal" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="glutenFree"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Gluten Free?</FormLabel>
+                <FormControl className="flex gap-4">
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange as () => void}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
         </div>
-      </div>
-      <Button type="submit">Create Recipe</Button>
-    </form>
+
+        <Button type="submit">Create Recipe</Button>
+      </form>
+    </Form>
   );
 }
