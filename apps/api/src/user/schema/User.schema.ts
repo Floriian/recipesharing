c249437933f4ba726a587a710da6fef4fa8e608b
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-20 23:57:23 +0200

    add recipe field to userschema

--- apps/api/src/user/schema/User.schema.ts
+++ apps/api/src/user/schema/User.schema.ts
@@ -1,18 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IRecipe, IUser } from '@recipe-sharing/types';
 import mongoose, { HydratedDocument, Model } from 'mongoose';
 import { Recipe } from '../../recipes/schema/Recipe.schema';
 
 @Schema()
-export class User implements IUser {
+export class User {
   @Prop({ required: true })
   sub: string;
 
   @Prop({ required: true })
   name: string;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Recipe.name })
-  recipes: IRecipe[];
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'recipes' })
+  recipes: Recipe[];
 }
 
 export type UserDocument = HydratedDocument<User>;
