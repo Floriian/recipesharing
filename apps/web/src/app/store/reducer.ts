407eaca719497edf7d42caaff976f78424c75d9f
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-23 15:49:23 +0200

    add userslice to rootreducer

--- apps/web/src/app/store/reducer.ts
+++ apps/web/src/app/store/reducer.ts
@@ -1,7 +1,9 @@
 import { recipeSlice } from "@/features/Recipe/recipe.slice";
+import { userSlice } from "@/features/User/user.slice";
 import { authenticationSlice } from "@/features/authentication/authenticationSlice";
 import { combineReducers } from "@reduxjs/toolkit";
 export const rootReducer = combineReducers({
   [recipeSlice.name]: recipeSlice.reducer,
   [authenticationSlice.name]: authenticationSlice.reducer,
+  [userSlice.name]: userSlice.reducer,
 });
