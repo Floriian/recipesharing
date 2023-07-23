74490887ad313abca4bb56907b894dc3327ee2c3
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-23 15:39:34 +0200

    use BaseResponseInterceptor in UserController.

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { GetUser } from 'src/decorators';
 import { Auth0Payload } from 'src/types';
+import { BaseResponseInterceptor } from 'src/interceptors';
 
 @Controller('user')
+@UseInterceptors(BaseResponseInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
