dae5c7cf6c7d6dd9c7a1b7df321f3d47a1f86ded
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-23 15:44:09 +0200

    add profile menu item

--- apps/web/src/components/layout/AuthenticatedMenu.tsx
+++ apps/web/src/components/layout/AuthenticatedMenu.tsx
@@ -9,8 +9,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 export function AuthenticatedMenu() {
   const { user, logout } = useAuth0();
+  const navigate = useNavigate();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -23,6 +25,12 @@ export function AuthenticatedMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuPortal>
         <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() => navigate("/user")}
+            className="hover:cursor-pointer"
+          >
+            Profile
+          </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem
             onClick={() => logout()}
