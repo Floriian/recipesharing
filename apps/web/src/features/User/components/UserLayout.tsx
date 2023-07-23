0b902564f267adc5ca8c3334d4000ff2779e8f27
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-23 15:44:15 +0200

    create userlayout

---
+++ apps/web/src/features/User/components/UserLayout.tsx
@@ -0,0 +1,11 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode;
+};
+export function UserLayout({ children }: Props) {
+  const { isAuthenticated } = useAuth0();
+  if (!isAuthenticated) return <Navigate to="/" />;
+  return <div className="flex w-full justify-center">{children}</div>;
+}
